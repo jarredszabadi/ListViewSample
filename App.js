f1352a36ce377cfe4f58b9caae13c38794f6f514
Jarred Szabadi <jarred.szabadi@shopify.com>
2018-07-19 11:02:52 -0400

    initial listview with netowkr and hardcoded values

---
+++ App.js
@@ -0,0 +1,59 @@
+import FlatListBasics from './app/FlatListBasics';
+export default FlatListBasics;
+
+// import React from 'react';
+// import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+//
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//       <ScrollView
+//         horizontal = {true}
+//         pagingEnabled = {true}>
+//         <Text style={{fontSize:96}}>Scroll me plz</Text>
+//         <Text style={{fontSize:96}}>If you like</Text>
+//
+//         <Text style={{fontSize:96}}>If you like</Text>
+//         <Text style={{fontSize:96}}>If you like</Text>
+//
+//         <Text style={{fontSize:96}}>Scrolling down</Text>
+//         <Text style={{fontSize:96}}>If you like</Text>
+//
+//         <Text style={{fontSize:96}}>Whats the best</Text>
+//         <Text style={{fontSize:96}}>If you like</Text>
+//
+//         <Text style={{fontSize:96}}>Framework around?</Text>
+//         <Text style={{fontSize:96}}>If you like</Text>
+//
+//         <Text style={{fontSize:80}}>React Native</Text>
+//         <Text style={{fontSize:96}}>Scroll me plz</Text>
+//         <Text style={{fontSize:96}}>If you like</Text>
+//
+//         <Text style={{fontSize:96}}>If you like</Text>
+//         <Text style={{fontSize:96}}>If you like</Text>
+//
+//         <Text style={{fontSize:96}}>Scrolling down</Text>
+//         <Text style={{fontSize:96}}>If you like</Text>
+//
+//         <Text style={{fontSize:96}}>Whats the best</Text>
+//         <Text style={{fontSize:96}}>If you like</Text>
+//
+//         <Text style={{fontSize:96}}>Framework around?</Text>
+//         <Text style={{fontSize:96}}>If you like</Text>
+//
+//         <Text style={{fontSize:80}}>React Native</Text>
+//       </ScrollView>
+//       </View>
+//     );
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
